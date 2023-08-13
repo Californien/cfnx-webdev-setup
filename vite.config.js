813a1c3d37b053c4211838420ef342b4564ba59b
Californien <marlin.lefler29@gmail.com>
2023-08-13 12:10:13 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+export default defineConfig({
+    root: 'src',
+    assetsInclude: ['./assets/**/*.*'],
+    base: '',
+    build: {
+        emptyOutDir: true,
+        outDir: '../dist',
+    }
+})
