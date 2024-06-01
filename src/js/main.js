fb42a7e45987a4c797d39f3db67fb80765a27a6a
Californien <marlin.lefler29@gmail.com>
2024-06-01 23:45:42 +0200

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,4 @@
 import '@/scss/main.scss';
+import '@/ts/main.ts';
+
+console.log('Hello from JS!');
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,30 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-            '#': path.resolve(__dirname, './src/assets')
-        }
-    },
-    root: 'src',
-    assetsInclude: ['./assets/**/*.*'],
-    base: '',
-    build: {
-        emptyOutDir: true,
-        outDir: '../dist',
-    }
+	test: {
+		include: ['**/js/*.test.js', '**/ts/*.test.ts'],
+		globals: true,
+		reporters: 'verbose',
+		environment: 'jsdom'
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'#': path.resolve(__dirname, './src/assets')
+		}
+	},
+	root: 'src',
+	assetsInclude: ['./assets/**/*.*'],
+	base: '',
+	build: {
+		emptyOutDir: true,
+		outDir: '../dist'
+	},
+	css: {
+		postcss: {
+			plugins: [autoprefixer({})]
+		}
+	}
 });
