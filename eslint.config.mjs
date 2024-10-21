872cb3e005fb9e95d573d06533210f8e16805d57
Californien <marlin.lefler29@gmail.com>
2024-10-21 15:48:00 +0200

    eslint config rework

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
-import globals from 'globals';
+// @ts-check
+
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import pluginJs from '@eslint/js';
+import globals from 'globals';
 
 export default tseslint.config(
 	{ languageOptions: { globals: globals.browser } },
-	pluginJs.configs.recommended,
+	eslint.configs.recommended,
 	...tseslint.configs.recommended
 );
